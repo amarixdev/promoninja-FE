8f8c93f864d06a6978a2dc7e55dd3a55ad508fd3
amarixdev <amaridev@alumni.unc.edu>
2023-02-05 04:54:25 -0500

    add accessToken to Apollo context

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,33 +3,63 @@ import { startStandaloneServer } from "@apollo/server/standalone";
 import { prisma, PrismaClient } from "@prisma/client";
 import resolvers from "./graphql/resolvers.ts";
 import typeDefs from "./graphql/typeDefs";
+import Axios from "axios";
+import express from "express";
+import * as dotenv from "dotenv";
+import { createServer } from "http";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import cors from "cors";
+import { env } from "../environment";
+import fetch from "node-fetch";
 
-// The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
+const { CLIENT_ID, CLIENT_SECRET } = env;
 
 const main = async () => {
+  const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers,
+  });
+
   interface MyContext {
     prisma?: PrismaClient;
     test: String;
   }
 
   const server = new ApolloServer<MyContext>({
-    resolvers,
-    typeDefs,
+    schema,
   });
+
   const prisma = new PrismaClient();
   const PORT = 4000;
 
   const { url } = await startStandaloneServer(server, {
-    context: async () => {
+    context: async ({ req, res }) => {
+      const accessToken = await fetch(
+        "https://accounts.spotify.com/api/token",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            Authorization:
+              "Basic " +
+              Buffer.from(CLIENT_ID + ":" + CLIENT_SECRET).toString("base64"),
+          },
+          body: "grant_type=client_credentials",
+        }
+      )
+        .then((result) => result.json())
+        .then((data) => {
+          return data.access_token;
+        });
       return {
+        accessToken,
         test: "test",
         prisma,
       };
     },
+
     listen: { port: PORT },
   });
-
   console.log(`🚀  Server ready at: ${url}`);
 };
 
