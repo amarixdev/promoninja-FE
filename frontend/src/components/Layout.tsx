04f083a202adb8ba923302c01eb5c258caf2238e
amarixdev <amaridev@alumni.unc.edu>
2023-02-05 09:43:20 -0500

    add simple UI to render podcasts

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { createContext, useContext } from "react";
 import Header from "./Header";
 import Hero from "./Hero";
 import dotenv from "dotenv";
 import gql from "graphql-tag";
 import { useLazyQuery, useQuery } from "@apollo/client";
 import { Button } from "@chakra-ui/react";
+import APIContext from "../context/context";
+import Image from "next/image";
+import Link from "next/link";
+("../context/context");
 
 type Props = {};
 
@@ -13,45 +17,67 @@ interface Item {
 }
 
 const Layout = (props: Props) => {
+  const { offset, setOffset } = useContext(APIContext);
+
   const QUERY = gql`
-    query {
-      shows {
+    query ($input: Int) {
+      shows(input: $input) {
         items {
           name
+          images {
+            url
+          }
+          external_urls {
+            spotify
+          }
         }
       }
     }
   `;
 
-  const { data, loading, error } = useQuery(QUERY);
-
-  const [getPodcasts, {data: newData }] = useLazyQuery(QUERY);
+  const [getPodcasts, { data, loading, error }] = useLazyQuery(QUERY, {
+    variables: {
+      input: offset,
+    },
+  });
 
   const handlePodcasts = () => {
-
-    getPodcasts()
-  }
+    getPodcasts();
+    setOffset((prev: number) => prev + 50);
+  };
 
   if (loading) return <div>LOADING....</div>;
   if (error) return <div>ERROR</div>;
 
-  const {
-    shows: { items },
-  } = data;
+  const podcasts = data?.shows?.items;
 
-  console.log(items);
+  console.log(data?.shows?.items[0]?.external_urls.spotify);
 
   return (
     <>
       <Header />
       <Hero />
-      <Button onClick={handlePodcasts}>CLICK</Button>
-
-      {items.map((item: any, index: any) => (
-        <div key={index}>
-          <p>{item.name}</p>
+      <div className="w-full h-screen flex flex-col items-center">
+        <Button className="mb-10 p-8" onClick={handlePodcasts}>
+          Render Podcasts
+        </Button>
+        <div className="grid grid-cols-6 gap-5">
+          {podcasts?.map((item: any, index: any) => (
+            <div key={index} className="bg-black">
+              <p>{item.name}</p>
+              <Link href={item?.external_urls.spotify} target="_blank">
+                <Image
+                  src={item?.images[0]?.url}
+                  alt={item?.name}
+                  width={120}
+                  height={120}
+                  className="rounded-full"
+                />
+              </Link>
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
     </>
   );
 };
