b956bcd08b0b9a4a0112857d9302d6303dfb0ed1
amarixdev <amaridev@alumni.unc.edu>
2023-02-19 04:43:58 -0500

    send podcasts through prop

--- frontend/src/pages/podcasts.tsx
+++ frontend/src/pages/podcasts.tsx
@@ -12,7 +12,7 @@ import {
 import Image from "next/image";
 import Link from "next/link";
 import React, { useState } from "react";
-import Category from "../components/CategoryList";
+import CategoryList from "../components/CategoryList";
 import * as Hero from "../public/assets/comedy.png";
 import * as Logo from "../public/assets/ninja4.png";
 
@@ -20,7 +20,13 @@ type Props = {};
 
 const podcasts = (props: Props) => {
   const [category, setCategory] = useState({ name: "", id: "" });
-  const podcasts = [1, 2, 3, 4, 5];
+
+  const Comedy = Array.from({ length: 5 }, () => "Comedy");
+  const TopPicks = Array.from({ length: 5 }, () => "TopPicks");
+  const Technology = Array.from({ length: 5 }, () => "Technology");
+  const Politics = Array.from({ length: 5 }, () => "Politics");
+  const Lifestyle = Array.from({ length: 5 }, () => "Lifestyle");
+  const Educational = Array.from({ length: 5 }, () => "Educational");
 
   return (
     <div className="w-full bg-[#121212]">
@@ -35,12 +41,12 @@ const podcasts = (props: Props) => {
         </h1>
       </div>
       <div className=" w-full h-screen mt-12 ">
-        <Category category="Spotify's Top Picks" />
-        <Category category="Comedy" />
-        <Category category="Educational" />
-        <Category category="Technology" />
-        <Category category="News & Politics" />
-        <Category category="Lifestyle" />
+        <CategoryList category="Spotify's Top Picks" podcasts={TopPicks} />
+        <CategoryList category="Comedy" podcasts={Comedy} />
+        <CategoryList category="Educational" podcasts={Educational} />
+        <CategoryList category="Technology" podcasts={Technology} />
+        <CategoryList category="News & Politics" podcasts={Politics} />
+        <CategoryList category="Lifestyle" podcasts={Lifestyle} />
       </div>
     </div>
   );
