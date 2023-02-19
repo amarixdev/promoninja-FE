73a65a5b6da3c36aee83f9b313731343e4f8b34c
amarixdev <amaridev@alumni.unc.edu>
2023-02-19 03:27:28 -0500

    rm unused imports

--- frontend/src/graphql/apollo-client.ts
+++ frontend/src/graphql/apollo-client.ts
@@ -1,7 +1,4 @@
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
-import { setContext } from "@apollo/client/link/context";
-import APIContext from "../context/context";
-import { useContext } from "react";
 
 const GraphQLServer = "http://localhost:4000/graphql";
 
@@ -9,12 +6,9 @@ const httpLink = new HttpLink({
   uri: GraphQLServer,
 });
 
-
-
 const client = new ApolloClient({
   link: httpLink,
   cache: new InMemoryCache(),
-  
 });
 
 export default client;
