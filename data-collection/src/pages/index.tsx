5895a554df7e4771cec6a41e13fecaa5e5176acc
amarixdev <amaridev@alumni.unc.edu>
2023-02-20 08:18:52 -0500

    createPodcast GQL mutation
    conditionally redner UX based on creating or updating podcast

--- data-collection/src/pages/index.tsx
+++ data-collection/src/pages/index.tsx
@@ -5,17 +5,22 @@ import { Button, Input, Text, VStack, useToast } from "@chakra-ui/react";
 import CreateSponsor from "../components/CreateSponsor";
 import AppContext from "../context/context";
 import { Select } from "@chakra-ui/react";
-import { useQuery } from "@apollo/client";
+import { useMutation, useQuery } from "@apollo/client";
 import { gql } from "graphql-tag";
+import { Operations } from "../graphql/operations";
 
 const App = () => {
   const toast = useToast();
   const [category, setCategory] = useState("");
   const [podcast, setPodcast] = useState("");
   const [displaySponsor, setDisplaySponsor] = useState(false);
+  const [displaySubmit, setDisplaySubmit] = useState(false);
   const [text, setText] = useState("");
   const { sponsor } = useContext(AppContext);
   const [searchPreview, setSearchPreview] = useState([]);
+  const [createPodcast, { loading, error }] = useMutation(
+    Operations.Mutations.CreatePodcast
+  );
 
   const { data } = useQuery(gql`
     query {
@@ -27,45 +32,60 @@ const App = () => {
 
   const handleInputChange = async (e: any) => {
     try {
+      console.log(data)
       setText(e.target.value);
       const podcasts = data.getPodcasts.map((obj: any) => obj.title);
       const preview = podcasts.filter((title: string) =>
         title.toLowerCase().includes(text.toLowerCase())
       );
-
+      setDisplaySubmit(false);
+      setDisplaySponsor(false);
       setSearchPreview(preview);
-      console.log(searchPreview);
+      setPodcast("");
     } catch (error) {
       console.log(error);
     }
   };
 
-  const handleSubmit = async (preview: string) => {
+  const handleSubmit = async (
+    e: any,
+    preview: string,
+    existingPodcast: boolean
+  ) => {
+    e.preventDefault();
+    setText(preview);
     setPodcast(preview);
-
+    setSearchPreview([]);
     setDisplaySponsor(true);
+
+    if (!existingPodcast) {
+      setDisplaySubmit(true);
+    }
+
+    if (!existingPodcast && text === podcast) {
+      setDisplaySubmit(false);
+    }
+
+    if (!existingPodcast && displaySubmit) {
+      setDisplaySubmit(true);
+    }
+
     try {
       if (podcast === "") {
-        console.log("ERROR");
-
         return;
       } else {
       }
-      setText("");
     } catch (error) {
       console.log(error);
     }
   };
 
-  /* Send data to server-endpoint via POST request */
-
   const handleSave = async () => {
     const sponsorReq = false;
+
     try {
-      const response = await axios.post("/api/create-podcast", {
-        podcast,
-        category,
-        sponsorReq,
+      createPodcast({
+        variables: { input: { podcast, category } },
       });
 
       toast({
@@ -75,11 +95,7 @@ const App = () => {
         duration: 3000,
         isClosable: true,
       });
-      // console.log(response.data);
-      setPodcast("");
     } catch (error: any) {
-      // const { message } = error.response.data;
-
       toast({
         title: "Error.",
         description: error.response.data.message.podcast,
@@ -87,66 +103,88 @@ const App = () => {
         duration: 3000,
         isClosable: true,
       });
-      setPodcast("");
     }
+    setCategory("");
+    setText("");
+    setPodcast("");
   };
 
   return (
     <div className="bg-[#1e1e1e] h-screen w-full flex flex-col items-center justify-center">
       {/* Create Podcast and Sponsor page */}
-      <h1 className="text-white font-semibold text-5xl mb-4">{podcast}</h1>
+      <h1 className="text-white font-semibold text-3xl sm:text-4xl lg:text-5xl mb-4 fixed top-10">
+        {podcast}
+      </h1>
       <form
-        className="w-[500px] flex flex-col justify-center items-center mb-4"
+        onSubmit={(e) => handleSubmit(e, text, false)}
+        className="w-[500px] flex flex-col justify-center items-center mb-4 h-[500px]"
       >
-        <VStack spacing={5}>
-          <Text color={"white"}>Podcast Title</Text>
-          <Input
-            type="text"
-            value={text}
-            w={200}
-            color={"white"}
-            onChange={(e) => handleInputChange(e)}
-          />
-          <div className="w-[300px] bg-[#12121] flex flex-col items-center">
-            <ul className="text-center">
-              {text &&
-                searchPreview.map((preview) => (
-                  <li key={preview}>
-                    <Button onClick={() => handleSubmit(preview)}>
-                      {preview}
-                    </Button>
-                  </li>
-                ))}
-            </ul>
+        <VStack spacing={5} className="h-full">
+          <div className=" w-full flex-col items-center justify-center text-center">
+            <Text color={"white"} mt={10}>
+              Podcast Title
+            </Text>
+            <Input
+              type="text"
+              value={text}
+              w={200}
+              color={"white"}
+              onChange={(e) => handleInputChange(e)}
+              mt={10}
+            />
+            <div className="w-[300px] bg-[#12121] flex flex-col items-center mt-10">
+              <ul className="text-center">
+                {text &&
+                  searchPreview.map((preview) => (
+                    <li key={preview}>
+                      <Button
+                        onClick={(e) => handleSubmit(e, preview, true)}
+                        margin={1}
+                      >
+                        {preview}
+                      </Button>
+                    </li>
+                  ))}
+              </ul>
+            </div>
           </div>
-
           {displaySponsor && podcast ? (
-            <CreateSponsor podcast={podcast} />
+            <CreateSponsor
+              podcast={podcast}
+              createPodcast={createPodcast}
+              displaySubmit={displaySubmit}
+              category={category}
+            />
           ) : null}
 
-          <Select
-            placeholder="--Select Category--"
-            textColor={"white"}
-            textAlign={"center"}
-            onChange={(e) => setCategory(e.target.value)}
-          >
-            <option value="comedy">Comedy</option>
-            <option value="technology">Technology</option>
-            <option value="news & politics">News & Politics</option>
-            <option value="education">Education</option>
-            <option value="lifestyle">Lifestyle</option>
-            <option value="Science">Science</option>
-            <option value="Sports">Sports</option>
-          </Select>
-          <Button
-            type="submit"
-            colorScheme={"purple"}
-            w={"300px"}
-            mt={10}
-            p={4}
-          >
-            Add to Database
-          </Button>
+          {displaySubmit && (
+            <Select
+              placeholder="--Select Category--"
+              textColor={"white"}
+              textAlign={"center"}
+              onChange={(e) => setCategory(e.target.value)}
+              value={category}
+            >
+              <option value="comedy">Comedy</option>
+              <option value="technology">Technology</option>
+              <option value="news & politics">News & Politics</option>
+              <option value="education">Education</option>
+              <option value="lifestyle">Lifestyle</option>
+              <option value="science">Science</option>
+              <option value="sports">Sports</option>
+            </Select>
+          )}
+          {displaySubmit && (
+            <Button
+              onClick={handleSave}
+              colorScheme={"purple"}
+              w={"300px"}
+              mt={10}
+              p={4}
+            >
+              Add to Database
+            </Button>
+          )}
         </VStack>
       </form>
     </div>
