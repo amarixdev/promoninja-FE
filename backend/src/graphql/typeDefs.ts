6a71228bd3695c973878367e3916ab701065ce49
amarixdev <amaridev@alumni.unc.edu>
2023-02-21 03:43:32 -0500

    update types

--- backend/src/graphql/typeDefs.ts
+++ backend/src/graphql/typeDefs.ts
@@ -2,36 +2,53 @@ import gql from "graphql-tag";
 
 const typeDefs = gql`
   type Query {
-    podcast: [Podcast]
-    sponsor: [Sponsor]
+    getPodcasts: [Podcast]
+    getSponsors(input: PodcastInput!): [Sponsor]
   }
 
   type Mutation {
-    createPodcast: Boolean
+    createPodcast(input: PodcastInput!): Boolean
     updatePodcast: Boolean
+    createSponsor(input: PodcastInput!): Boolean
+    updateSponsor: Boolean
+  }
+
+  input PodcastInput {
+    sponsor: SponsorInput
+    podcast: String!
+    category: String
+  }
+
+  input SponsorInput {
+    name: String!
+    url: String
+    description: String
+    image: String
   }
 
   type Podcast {
     id: ID!
     title: String!
     imageUrl: String
-    sponsors: [Sponsor]
-    category: [Category]
-    offer: [PodcastSponsor]
+    categoryId: ID
+    offer: [Offer]
+    sponsorId: [ID]
   }
 
   type Category {
     id: ID!
     name: String!
   }
-
+  type Offer {
+    sponsor: String!
+    description: String!
+  }
   type Sponsor {
     id: ID!
     name: String!
     imageUrl: String
-    podcasts: [Sponsor]
+    podcastsId: [ID]
   }
-
 `;
 
 export default typeDefs;
