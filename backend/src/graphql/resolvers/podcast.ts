323328cb2cf9255cf844dc583c77b9a0e936399b
amarixdev <amaridev@alumni.unc.edu>
2023-02-24 06:20:41 -0500

    fetchSpotifyPodcast logic

--- backend/src/graphql/resolvers/podcast.ts
+++ backend/src/graphql/resolvers/podcast.ts
@@ -1,5 +1,6 @@
 import { prisma } from "@prisma/client";
-import { GraphQLContext, PodcastInput } from "../../util/types";
+import { GraphQLContext, PodcastInput, SpotifyAPI } from "../../util/types";
+import fetch from "node-fetch";
 
 export const podcastResolvers = {
   Mutation: {
@@ -10,7 +11,7 @@ export const podcastResolvers = {
     ) => {
       const { prisma } = context;
 
-      let { podcast, category } = input;
+      let { podcast, category, image } = input;
 
       category = category?.toLowerCase();
 
@@ -23,6 +24,7 @@ export const podcastResolvers = {
       const createdPodcast = await prisma.podcast.create({
         data: {
           title: podcast,
+          imageUrl: image,
           category: {
             connect: {
               id: CATEGORY?.id,
@@ -39,11 +41,36 @@ export const podcastResolvers = {
       const { prisma } = context;
       try {
         const podcasts = await prisma.podcast.findMany({
-          take: 10,
+          take: 6,
         });
         return podcasts;
       } catch (error) {
         console.log(error);
+      }
+    },
+    fetchSpotifyPodcast: async (
+      parent: any,
+      { input }: SpotifyAPI,
+      context: GraphQLContext
+    ) => {
+      const { accessToken, prisma } = context;
+      const { podcast } = input;
+      console.log(podcast);
+
+      try {
+        const result = await fetch(
+          `https://api.spotify.com/v1/search?q=${podcast}&type=show&market=US&limit=1`,
+          {
+            method: "GET",
+            headers: { Authorization: "Bearer " + accessToken },
+          }
+        );
+
+        const data = await result.json();
+        console.log(data.shows.items.name);
+        return data?.shows?.items;
+      } catch (err) {
+        console.log(err);
       }
     },
   },
