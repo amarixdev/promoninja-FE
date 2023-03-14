d4e5c7fccd11828e5467c66f70863f2ac8c7f915
amarixdev <amaridev@alumni.unc.edu>
2023-03-14 05:08:19 -0400

    remove unused context api

--- data-collection/src/pages/_app.tsx
+++ data-collection/src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
-import { ContextProvider } from "../context/context";
+// import { ContextProvider } from "../context/context";
 import "../styles/globals.css";
 import {
   ApolloClient,
@@ -8,17 +8,15 @@ import {
   HttpLink,
   InMemoryCache,
 } from "@apollo/client";
-import client from "./graphql/apollo-client";
+import client from "../graphql/apollo-client";
 import theme from "../styles/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ApolloProvider client={client}>
-      <ContextProvider>
-        <ChakraProvider theme={theme}>
-          <Component {...pageProps} />
-        </ChakraProvider>
-      </ContextProvider>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
     </ApolloProvider>
   );
 }
