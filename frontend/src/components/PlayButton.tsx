a5fadbcd02201fb4621bcfdf8a54b2c226a5a814
amarixdev <amaridev@alumni.unc.edu>
2023-03-14 04:58:41 -0400

    style: re-design podcast page UI/UX
    play button now displaying sponsors. Add dynamic background theme. Add call to action

--- frontend/src/components/PlayButton.tsx
+++ frontend/src/components/PlayButton.tsx
@@ -1,4 +1,4 @@
-import { Button, useDisclosure } from "@chakra-ui/react";
+import { useDisclosure } from "@chakra-ui/react";
 import Image from "next/image";
 import React, { useState } from "react";
 import styles from "../../styles/style.module.css";
@@ -8,44 +8,73 @@ import { FaForward, FaBackward } from "react-icons/fa";
 
 interface Props {
   sponsorData: SponsorData[];
+  isActive: boolean;
+  setIsActive: any;
+  handleScrub: (forward: boolean) => void;
+  sponsorIndex: number;
 }
 
-const PlayButton = ({ sponsorData }: Props) => {
-  const [sponsorName, setSponsorName] = useState("");
-
-  const [isActive, setIsActive] = useState(false);
+const PlayButton = ({
+  sponsorData,
+  isActive,
+  setIsActive,
+  handleScrub,
+  sponsorIndex,
+}: Props) => {
   return (
-    <div className="flex flex-col items-center justify-center mt-4">
-      <div
-        className="w-full flex flex-col items-center justify-center cursor-pointer"
-        onClick={() => setIsActive((prev) => !prev)}
-      >
-        <div
-          className={`${styles.play} ${
-            isActive && styles.clicked
-          } mt-4 w-[40px] h-[40px] bg-white cursor-pointer transition-all duration-300 ease-in-out`}
-        ></div>
-        {isActive || <h1 className="font-bold py-4 mt-2">Show Sponsors</h1>}
-      </div>
-      {isActive && (
+    <div className="flex flex-col items-center justify-center mt-4 absolute base:top-[320px] xs:top-[340px] sm:top-[360px] ">
+      {
         <div className=" mt-2 p-6 flex flex-col items-center justify-center">
-          {sponsorData.map((sponsor) => (
-            <div className="flex justify-evenly">
-              <FaBackward size={50} className="mt-14 mr-10 active:scale-95" />
-              <div className="flex flex-col items-center justify-center w-full">
-                <Image
-                  src={sponsor.imageUrl || Logo}
-                  width={140}
-                  height={140}
-                  alt="/"
-                  className="rounded-3xl xs:w-[140px] base:w-[110px] sm:w-[220px]"
-                />
-                <p className="mt-3 font-semibold">{sponsor.name}</p>
+          <>
+            {isActive || (
+              <div className="flex justify-evenly">
+                <div className="flex flex-col items-center justify-center w-full">
+                  <Image
+                    src={sponsorData[sponsorIndex].imageUrl || Logo}
+                    width={140}
+                    height={140}
+                    alt="/"
+                    className="rounded-3xl xs:w-[140px] base:w-[110px] sm:w-[180px] cursor-pointer"
+                    onClick={() => setIsActive((prev: any) => !prev)}
+                  />
+                  <p className="mt-3 font-bold text-sm whitespace-nowrap">
+                    {sponsorData[sponsorIndex].name}
+                  </p>
+                </div>
               </div>
-              <FaForward size={50} className="mt-14 ml-10 active:scale-95" />
-            </div>
-          ))}
+            )}
+          </>
         </div>
+      }
+      {/* Play Button */}
+
+      <div className="flex justify-evenly min-w-[60vw] absolute base:top-[180px] xs:top-[200px] sm:top-[220px] my-2">
+        <FaBackward
+          size={40}
+          className="mt-6 xs:mt-14 base:w-[25px] xs:w-[40px] active:scale-95 mr-14 cursor-pointer"
+          onClick={() => handleScrub(false)}
+          color={""}
+        />
+        <div
+          className="w-fit flex flex-col items-center justify-center cursor-pointer base:mt-2 xs:mt-10"
+          onClick={() => setIsActive((prev: any) => !prev)}
+        >
+          <div
+            className={`${styles.play} ${
+              isActive && styles.clicked
+            } mt-4 base:w-[25px] base:h-[25px] xs:w-[40px] xs:h-[40px] bg-white cursor-pointer transition-all duration-300 ease-in-out`}
+          ></div>
+        </div>
+        <FaForward
+          size={40}
+          className="mt-6 xs:mt-14 base:w-[25px] xs:w-[40px] active:scale-95 ml-14 cursor-pointer"
+          onClick={() => handleScrub(true)}
+        />
+      </div>
+      {isActive || (
+        <span className="w-full text-center text-xs relative bottom-3 font-medium">
+          {sponsorIndex + 1}/{sponsorData.length}
+        </span>
       )}
     </div>
   );
--- frontend/src/pages/podcasts/[category]/[podcast].tsx
+++ frontend/src/pages/podcasts/[category]/[podcast].tsx
@@ -1,11 +1,19 @@
 // import ColorExtractor from "../../../components/ColorExtractor";
-import { Img, Spinner } from "@chakra-ui/react";
+import { Divider, Img, Spinner } from "@chakra-ui/react";
 import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
 import Footer from "../../../components/Footer";
 import PlayButton from "../../../components/PlayButton";
 import Sidebar from "../../../components/Sidebar";
 import client from "../../../graphql/apollo-client";
 import { Operations } from "../../../graphql/operations";
+import {
+  callToAction,
+  convertToFullURL,
+  rgbToHex,
+  truncateString,
+} from "../../../utils/functions";
 import { useMediaQuery } from "../../../utils/hooks";
 import { PodcastData, SponsorData } from "../../../utils/types";
 
@@ -18,36 +26,124 @@ interface Props {
 const podcast = ({ podcastData, sponsorData }: Props) => {
   const imageSrc = podcastData?.imageUrl;
   const isBreakPoint = useMediaQuery(1023);
+  const [isActive, setIsActive] = useState(false);
+  const [sponsorIndex, setSponsorIndex] = useState(0);
 
-  console.log(sponsorData);
+  const handleScrub = (forward: boolean) => {
+    const limit = sponsorData?.length - 1;
+
+    /* Scrub Forward */
+    if (forward && sponsorIndex !== limit) {
+      setSponsorIndex((prev) => prev + 1);
+
+      /* Scrub Backward */
+    }
+    if (forward && sponsorIndex === limit) {
+      setSponsorIndex(0);
+    }
+    if (!forward && sponsorIndex === 0) {
+      setSponsorIndex(sponsorData.length - 1);
+    } else if (!forward && sponsorIndex !== 0) {
+      setSponsorIndex((prev) => prev - 1);
+    }
+  };
 
   if (!imageSrc)
     return (
-      <div className="flex w-full h-screen items-center justify-center">
+      <div className="flex w-full h-screen items-center justify-center ">
         <Spinner />
       </div>
     );
 
-  return (
-    <div className="flex flex-col items-center relative ">
-      <Image
-        src={imageSrc}
-        alt="/"
-        width={230}
-        height={230}
-        priority
-        className="z-10 rounded-3xl relative mt-10 xs:w-[160px] base:w-[130px] sm:w-[230px]"
-      />
-      <div className="w-11/12 flex flex-col justify-center">
-        <h1 className="text-lg font-bold text-center mt-10 px-4 base:text-xl xs:text-2xl sm:text-3xl">
-          {podcastData?.title}
-        </h1>
-        <p className="w-full text-md xs:text-lg font-semibold text-center mt-6">
-          {podcastData?.publisher}
-        </p>
-      </div>
-      <PlayButton sponsorData={sponsorData} />
+  const currentPodcast = podcastData.offer.filter((offer) =>
+    offer.sponsor.includes(sponsorData[sponsorIndex].name)
+  );
 
+  const backgroundColor = podcastData.backgroundColor;
+  const gradientStyle = {
+    backgroundImage: `linear-gradient(to bottom, ${backgroundColor}, #101010)`,
+  };
+
+  return (
+    <div className={`${isBreakPoint ? "flex flex-col" : "flex"}`}>
+      <Sidebar />
+      <div className="flex flex-col items-center relative h-screen bg-[#101010] w-full">
+        {isActive && (
+          <h1 className="font-bold base:text-lg xs:text-2xl text-3xl mt-6">
+            {sponsorData[sponsorIndex]?.name}
+          </h1>
+        )}
+        <div
+          className={` items-center w-full flex justify-center`}
+          style={!isActive ? gradientStyle : undefined}
+        >
+          {isActive ? (
+            <Link
+              href={convertToFullURL(currentPodcast[0].url)}
+              target="_blank"
+            >
+              <Image
+                src={!isActive ? imageSrc : sponsorData[sponsorIndex]?.imageUrl}
+                alt="/"
+                width={230}
+                height={230}
+                priority
+                className="z-10 rounded-3xl relative mt-10 xs:w-[160px] base:w-[130px] sm:w-[190px] shadow-xl shadow-black"
+              />
+            </Link>
+          ) : (
+            <Image
+              src={!isActive ? imageSrc : sponsorData[sponsorIndex]?.imageUrl}
+              alt="/"
+              width={230}
+              height={230}
+              priority
+              className={`z-10 rounded-3xl relative mt-10 xs:w-[160px] base:w-[130px] sm:w-[190px]`}
+            />
+          )}
+        </div>
+        {isActive && (
+          <Link href={convertToFullURL(currentPodcast[0].url)} target="_blank">
+            <div className="font-semibold my-4 flex">
+              <p className="font-bold mx-2">Visit </p>
+              <p className="hover:underline hover:underline-offset-2">
+                {currentPodcast[0].url}
+              </p>
+            </div>
+          </Link>
+        )}
+        {!isActive ? (
+          <div className="w-10/12 flex flex-col justify-center">
+            <h1 className="text-lg font-bold text-center mt-10 px-4 base:text-xl xs:text-2xl sm:text-3xl">
+              {!isActive ? podcastData?.title : sponsorData[sponsorIndex]?.name}
+            </h1>
+            <p className="w-full text-md xs:text-lg font-semibold text-center mt-6">
+              {isActive || truncateString(podcastData?.publisher, 30)}
+            </p>
+          </div>
+        ) : (
+          <div className="w-10/12 text-center flex flex-col items-center justify-center mt-10">
+            <h1 className="font-semibold border border-1 border-green-500 p-4">
+              {truncateString(currentPodcast[0].description, 80)}
+            </h1>
+            <h1 className="font-bold base:text-md xs:text-lg sm:text-xl text-center my-10">
+              {callToAction(podcastData?.title)}
+            </h1>
+          </div>
+        )}
+        <PlayButton
+          sponsorData={sponsorData}
+          isActive={isActive}
+          setIsActive={setIsActive}
+          handleScrub={handleScrub}
+          sponsorIndex={sponsorIndex}
+        />
+      </div>
+      {isBreakPoint && (
+        <div className="w-full text-[#101010] base:text-[10px] xs:text-[5px] relative">
+          margin
+        </div>
+      )}
       <Footer />
     </div>
   );
@@ -84,7 +180,7 @@ export const getStaticProps = async ({ params }: any) => {
 
   podcastData = podcastData.getPodcast;
   sponsorData = sponsorData.fetchSponsors;
-  console.log(sponsorData);
+  console.log(podcastData);
   return {
     props: {
       podcastData,
