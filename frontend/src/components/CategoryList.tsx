4d0b6a1ae79a392c0a22de6510b8c96aa5650133
amarixdev <amaridev@alumni.unc.edu>
2023-04-10 04:10:17 -0400

    UI/UX improvements
    truncate podcast list to 8 per category; small responsiveness improvements

--- frontend/src/components/CategoryList.tsx
+++ frontend/src/components/CategoryList.tsx
@@ -26,7 +26,7 @@ const CategoryList = ({ category }: CategoryProps) => {
         isBreakPoint && "my-[-55px] xs:my-[-40px] sm:my-[-25px] md:my-[-15px]"
       }`}
     >
-      <div className={`flex justify-between items-center my-4 md:my-6 lg:my-4`}>
+      <div className={`flex justify-between items-center my-5 md:my-6 lg:my-4`}>
         <Link href={`/podcasts/${categoryName}`}>
           <div className="flex items-center justify-center">
             <h1
@@ -47,37 +47,45 @@ const CategoryList = ({ category }: CategoryProps) => {
           !isBreakPoint ? "ml-6" : "ml-2 bottom-10"
         }`}
       >
-        {podcastData?.map((podcast: PodcastData) => (
-          <Link
-            href={`/podcasts/${categoryName}/${podcast.title}`}
-            key={podcast.title}
+        {podcastData?.slice(0, 8).map((podcast: PodcastData) => (
+          <div
             className={
               !isBreakPoint
                 ? `from-[#0d0d0d] bg-gradient-radial to-[#202020] hover:bg-[#292727] hover:cursor-pointer flex flex-col items-center min-w-[180px] sm:min-w-[200px] md:min-w-[220px] lg:min-w-[240px] h-[255px] sm:h-[283px] md:h-[312px] lg:h-[340px] rounded-lg mx-3`
                 : "hover:cursor-pointer flex flex-col items-center min-w-[120px] md:min-w-[140px] h-[255px] sm:h-[285px] ml-2 rounded-lg overflow-y-visible sm:mx-5"
             }
+            key={podcast.title}
           >
-            <Image
-              src={podcast.imageUrl}
-              alt={podcast.title}
-              width={190}
-              height={190}
-              loading="lazy"
-              className="rounded-xl mt-4 shadow-lg shadow-black base:w-[70px] xs:w-[90px] sm:w-[150px] md:w-[170px] lg:w-[190px] "
-              onClick={handlePreviousPage}
-            />
+            <Link
+              href={`/podcasts/${categoryName}/${podcast.title}`}
+              key={podcast.title}
+            >
+              <Image
+                src={podcast.imageUrl}
+                alt={podcast.title}
+                width={190}
+                height={190}
+                loading="lazy"
+                className="rounded-xl mt-4 shadow-lg shadow-black base:w-[90px] xs:w-[110px] sm:w-[170px] md:w-[190px] lg:w-[190px] "
+                onClick={handlePreviousPage}
+              />
+            </Link>
 
-            <h1 className="text-sm sm:text-md lg:text-lg text-center px-2 pt-6 font-semibold text-white whitespace-nowrap">
-              {!isBreakPoint
-                ? truncateString(podcast.title, 20)
-                : truncateString(podcast.title, 15)}
-            </h1>
-            <p className="text-xs sm:text-sm lg:text-md text-center px-2 font-medium text-[#909090]">
-              {!isBreakPoint
-                ? podcast.publisher
-                : truncateString(podcast.publisher, 30)}
-            </p>
-          </Link>
+            <div>
+              <h1
+                className={`text-sm sm:text-md lg:text-lg base:text-left lg:text-center px-2 pt-6 font-semibold text-white whitespace-nowrap`}
+              >
+                {!isBreakPoint
+                  ? truncateString(podcast.title, 20)
+                  : truncateString(podcast.title, 14)}
+              </h1>
+              <p className="text-xs sm:text-sm lg:text-md base:text-left lg:text-center px-2 font-medium text-[#909090]">
+                {!isBreakPoint
+                  ? podcast.publisher
+                  : truncateString(podcast.publisher, 30)}
+              </p>
+            </div>
+          </div>
         ))}
       </div>
     </div>
