14649cc1e09f1b29c41c5cd0eddb8ea19c8481de
amarixdev <amaridev@alumni.unc.edu>
2023-04-25 06:13:33 -0400

    add function utils

--- frontend/src/utils/functions.ts
+++ frontend/src/utils/functions.ts
@@ -1,17 +1,48 @@
-export const capitalizeString = (str: string) => {
-    const result = str
-      .split(" ")
-      .map((word) => {
-        return word.charAt(0).toUpperCase() + word.slice(1);
-      })
-      .join(" ");
+export const capitalizeString = (str: string | undefined) => {
+  if (!str) return;
+  const result = str
+    .split(" ")
+    .map((word) => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    })
+    .join(" ");
 
-    return result;
+  return result;
 };
-  
 
 export const truncateString = (string: string, num: number) => {
   if (string?.length > num) {
     return string.slice(0, num) + `...`;
   } else return string;
+};
+
+export const convertToSlug = (title: string) => {
+  return title
+    .toLowerCase()
+    .replace(/ /g, "-")
+    .replace(/[^\w-]+/g, "");
+};
+
+export const convertToFullURL = (link: string) => {
+  if (link.startsWith("http://") || link.startsWith("https://")) {
+    return link;
+  } else {
+    return "http://" + link;
+  }
+};
+
+export const callToAction = (input: string) => {
+  let output = input.trim();
+  // remove "Podcast" at the end (if present)
+  if (output.toLowerCase().endsWith("podcast")) {
+    output = output.slice(0, output.length - 7).trim();
+  }
+  // check if the input already includes "the" (case-insensitive)
+  if (!output.toLowerCase().includes("the ")) {
+    output = "The " + output;
+  }
+  // add "Support" and "podcast" to the output
+  output = `Support ${output} podcast!`;
+
+  return output;
 };
