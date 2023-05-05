66659e48b7b168ac6a06040d2287329b16a11331
amarixdev <amaridev@alumni.unc.edu>
2023-05-05 00:06:19 -0400

    feat: add loading animation

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,18 +2,28 @@ import type { AppProps } from "next/app";
 import { ApolloProvider } from "@apollo/client";
 import client from "../graphql/apollo-client";
 import { ChakraProvider } from "@chakra-ui/react";
-import { theme } from "../../styles/chakra/theme";
+import { theme, ForceDarkMode } from "../../styles/chakra/theme";
 import "../../styles/globals.css";
 import { ContextProvider } from "../context/navContext";
+import style from "../../styles/style.module.css";
+import { useLoadingScreen } from "../utils/hooks";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const isLoading = useLoadingScreen();
   return (
     <ApolloProvider client={client}>
-      <ContextProvider>
-        <ChakraProvider theme={theme}>
-          <Component {...pageProps} />
-        </ChakraProvider>
-      </ContextProvider>
+      <ChakraProvider theme={theme}>
+        <ContextProvider>
+          {isLoading && (
+            <div className="h-screen fixed w-full flex top-0 z-[9999]">
+              <div className={`${style.loader}`} />
+            </div>
+          )}
+          <ForceDarkMode>
+            <Component {...pageProps} />
+          </ForceDarkMode>
+        </ContextProvider>
+      </ChakraProvider>
     </ApolloProvider>
   );
 }
