6efab60980c3a020200a92d0c65bba3743bef509
amarixdev <amaridev@alumni.unc.edu>
2023-05-05 02:44:24 -0400

    bug fix: fix Chakra darkmode theme

--- frontend/styles/chakra/theme.ts
+++ frontend/styles/chakra/theme.ts
@@ -1,5 +1,11 @@
-import { extendTheme } from "@chakra-ui/react";
+import { extendTheme, useColorMode, type ThemeConfig } from "@chakra-ui/react";
+import { useEffect } from "react";
+const config: ThemeConfig = {
+  initialColorMode: "dark",
+  useSystemColorMode: true,
+};
 export const theme = extendTheme({
+  config,
   styles: {
     global: () => ({
       body: {
@@ -7,4 +13,24 @@ export const theme = extendTheme({
       },
     }),
   },
+  colors: {
+    transparent: "transparent",
+    black: "#000",
+    white: "#fff",
+    gray: {
+      50: "#f7fafc",
+      900: "#171923",
+    },
+  },
 });
+
+export function ForceDarkMode(props: { children: JSX.Element }) {
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  useEffect(() => {
+    if (colorMode === "dark") return;
+    toggleColorMode();
+  }, [colorMode]);
+
+  return props.children;
+}
